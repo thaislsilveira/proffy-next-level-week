cf85e207fa5a3bdb23463b3132a7451bb7b4e336
thaislsilveira <thais_lsilveira>
2020-08-05 17:04:42 -0300

    List dos proffys

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,35 +4,43 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars3.githubusercontent.com/u/34002389?s=460&u=87794bfaceacc304760b3329b20af09ed10e2c1b&v=4"
-          alt="Thaís Silveira"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Thais Silveira</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name} </strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>Apaixonada por números.</p>
-      <br />
-      <br />
-      <p>Focada em sempre proporcionar o melhor conteúdo para seus alunos</p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 20,00 </strong>
+          <strong>R${teacher.cost} </strong>
         </p>
-        <button type="button">
+        <a href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="WhatsApp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
