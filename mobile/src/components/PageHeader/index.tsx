9d3616d3f813c0b5e0280f65415771f9e4e7629e
thaislsilveira <thais_lsilveira>
2020-08-07 10:07:00 -0300

    filtros de professores

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
@@ -11,9 +11,14 @@ import styles from './styles';
 
 interface PageHeaderProps {
   title: string;
+  headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  children,
+  headerRight,
+}) => {
   const { navigate } = useNavigation();
   function handleGoBack() {
     navigate('Landing');
@@ -28,7 +33,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
 
         <Image source={LogoImg} resizeMode="contain" />
       </View>
-      <Text style={styles.title}>{title}</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
+
+        {headerRight}
+      </View>
+
+      {children}
     </View>
   );
 };
