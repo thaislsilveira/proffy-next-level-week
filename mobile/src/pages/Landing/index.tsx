12d56f1dfef34680bbb996aac0be9faf331e95f1
thaislsilveira <thais_lsilveira>
2020-08-07 10:13:36 -0300

    conectando com a api

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../../services/api';
+
 import styles from './styles';
 
 import landingImg from '../../assets/images/landing.png';
@@ -11,6 +14,15 @@ import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
   const { navigate } = useNavigation();
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then((response) => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
 
   function handleNavigateToFiveClassesPage() {
     navigate('GiveClasses');
@@ -48,7 +60,8 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões já realizadas <Image source={heartIcon} />
+        Total de {totalConnections} conexões já realizadas{' '}
+        <Image source={heartIcon} />
       </Text>
     </View>
   );
